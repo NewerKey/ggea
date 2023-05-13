12cdd2e8a0a20669a4f6b3586f6dc451b2443412
Josef <josef.leinweber@code.berlin>
2023-05-13 17:49:05 +0200

    feat(acc route & CRUD): updated delete route and CRUD methods, to require the username to successfully delete your ac count & deleted redundant routes and methods

--- backend/src/api/routes/account.py
+++ backend/src/api/routes/account.py
@@ -72,24 +72,18 @@ async def retrieve_current_account_by_id(
 
 
 @router.put(
-    path="/update/{id}",
-    name="accounts:update-current-account_by_id",
+    path="",
+    name="accounts:update-current-account",
     response_model=AccountInResponse,
     status_code=fastapi.status.HTTP_200_OK,
 )
 async def edit_current_user_by_id(
-    id: int,
     account_update: AccountInUpdate,
     current_account: Account = fastapi.Depends(get_auth_current_user()),
     account_crud: AccountCRUDRepository = fastapi.Depends(
         get_crud(AccountCRUDRepository)
     ),
 ) -> AccountInResponse:
-    if id != current_account.id:
-        raise await http_exc_403_forbidden_request(
-            error_msg="Not authorized to access this account"
-        )
-
     if (
         account_update.username and account_update.username != current_account.username
     ) or (account_update.email and account_update.email != current_account.email):
@@ -99,50 +93,30 @@ async def edit_current_user_by_id(
             raise await http_exc_400_bad_request(
                 error_msg="Username or email already taken"
             )
-
-    updated_db_account = await account_crud.update_account(
-        AccountInRead(id=current_account.id), account_update=account_update
-    )
-    jwt_token = jwt_manager.generate_jwt(account=updated_db_account)
-
-    return AccountInResponse(
-        authorized_account=AccountWithToken(
-            token=jwt_token,
-            hashed_password=updated_db_account.hashed_password,
-            **updated_db_account.__dict__
-        ),
-    )
-
-
-@router.delete(
-    path="/delete/{id}",
-    name="accounts:delete-current-account_by_id",
-    response_model=AccountInDeletionResponse,
-    status_code=fastapi.status.HTTP_202_ACCEPTED,
-)
-async def remove_current_account_by_id(
-    id: uuid.UUID,
-    current_account: Account = fastapi.Depends(get_auth_current_user()),
-    account_crud: AccountCRUDRepository = fastapi.Depends(
-        get_crud(AccountCRUDRepository)
-    ),
-) -> AccountInDeletionResponse:
-    if id != current_account.id:
-        raise await http_exc_403_forbidden_request(
-            error_msg="Not authorized to access this account"
+    try:
+        updated_db_account = await account_crud.update_account(
+            AccountInRead(id=current_account.id), account_update=account_update
         )
+        jwt_token = jwt_manager.generate_jwt(account=updated_db_account)
 
-    is_account_deleted = await account_crud.delete_account_by_id(id=id)
-    return AccountInDeletionResponse(is_deleted=is_account_deleted)
+        return AccountInResponse(
+            authorized_account=AccountWithToken(
+                token=jwt_token,
+                hashed_password=updated_db_account.hashed_password,
+                **updated_db_account.__dict__
+            ),
+        )
+    except BaseException as e:
+        raise await http_exc_500_internal_server_error(error_msg=e.error_msg)
 
 
 @router.delete(
-    path="/delete/{username}",
-    name="accounts:delete-current-account-by-username",
+    path="/{username}",
+    name="accounts:delete-current",
     response_model=AccountInDeletionResponse,
     status_code=fastapi.status.HTTP_202_ACCEPTED,
 )
-async def remove_current_account(
+async def remove_curren(
     username: str,
     current_account: Account = fastapi.Depends(get_auth_current_user()),
     account_crud: AccountCRUDRepository = fastapi.Depends(
@@ -150,12 +124,15 @@ async def remove_current_account(
     ),
 ) -> AccountInDeletionResponse:
     if username != current_account.username:
-        raise await http_exc_403_forbidden_request()
+        raise await http_exc_403_forbidden_request(
+            error_msg="Not authorized to access this account"
+        )
+    try:
+        is_account_deleted = await account_crud.delete_account(id=current_account.id)
+        return AccountInDeletionResponse(is_deleted=is_account_deleted)
 
-    is_account_deleted = await account_crud.delete_account_by_username(
-        username=username
-    )
-    return AccountInDeletionResponse(is_deleted=is_account_deleted)
+    except BaseException as e:
+        raise await http_exc_500_internal_server_error(error_msg=e.error_msg)
 
 
 @router.get(
